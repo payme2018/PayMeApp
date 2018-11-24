b98d5107c611d8770cfe2fcce4f8f497cd5c2504
dkalyanasundaram <dkalyanasundaram@INLTP-DKALYANA>
2018-11-24 12:49:22 +0530

    PayMe First look

---
+++ PayMe/PayMe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PayMe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PayMe/PayMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PayMe.Startup))]
+namespace PayMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
