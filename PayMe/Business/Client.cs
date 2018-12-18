40d762febe214f6c051b3d6c25ab261fc1fd251c
Kiran Kumar <Kiran Kumar@DESKTOP-OEO4H27>
2018-12-18 20:04:46 +0530

    client added

---
+++ PayMe/Business/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Business
+{
+    public class Client
+    {
+        [Key]
+        public int ID { get; set; }
+
+      public string ClientCode { get; set; }
+        public string ClientName { get; set; }
+        public string PrimaryContact { get; set; }
+        public string LocationInfo { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ PayMe/DAL/ClientManager.cs
@@ -0,0 +1,57 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ClientManager
+    {
+        public IEnumerable<Client> GetClients()
+        {
+            try
+            {
+                var connectionString = ConfigurationManager.AppSettings["PayMe-Connectionstring"];
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("GetClientList", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+
+                List<Client> clientList = new List<Client>();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Client client = new Client();
+                        client.ID = Convert.ToInt32(reader["ID"].ToString());
+                        client.LocationInfo = reader["LocationInfo"].ToString();
+                        client.Description = reader["Description"].ToString();
+                        client.PrimaryContact = reader["PrimaryContact"].ToString();
+                        client.IsActive = Convert.ToBoolean(reader["IsActive"].ToString());
+
+
+                        clientList.Add(client);
+                    }
+                }
+                reader.Close();
+                connection.Close();
+
+                return clientList;
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message.ToString());
+            }
+        }
+
+    }
+}
