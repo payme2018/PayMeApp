3b269cc799f622206683fdb4146b2498c4366506
Venkatesh Krishnasamy <Venkatesh.Krishnasamy@consilio.com>
2019-02-11 07:44:29 +0530

    Timesheet Report development

--- PayMe/DAL/TimesheetManager.cs
+++ PayMe/DAL/TimesheetManager.cs
@@ -57,6 +57,55 @@ namespace DAL
             }
         }
 
+
+
+        public IEnumerable<Timesheet> TimesheetDetailReport(string FromDate, string ToDate)
+        {
+            try
+            {
+                var connectionString = ConfigurationManager.AppSettings["PayMe-Connectionstring"];
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("GetTimesheetDetailReportByDate", connection);
+                cmd.Parameters.AddWithValue("@FromDate", FromDate);
+                cmd.Parameters.AddWithValue("@ToDate", ToDate);
+                cmd.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Timesheet> timesheetList = new List<Timesheet>();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Timesheet timesheet = new Timesheet();
+                        timesheet.ID = Convert.ToInt32(reader["ID"].ToString());
+                        timesheet.fkEmpId = Convert.ToInt32(reader["fkEmpId"]);
+                        timesheet.fkProjectID = Convert.ToInt32(reader["fkProjectID"]);
+                        timesheet.fkClientId = Convert.ToInt32(reader["fkClientId"]);
+                        timesheet.fkTaskID = Convert.ToInt32(reader["fkTaskID"]);
+                        timesheet.CheckInDate = Convert.ToDateTime(reader["CheckInDate"].ToString());
+                        timesheet.CheckInDateTime = Convert.ToDateTime(reader["CheckInDateTime"].ToString());
+                        timesheet.CheckOutDatetime = Convert.ToDateTime(reader["CheckOutDatetime"].ToString());
+                        timesheet.Description = reader["Description"].ToString();
+                        timesheet.ProjectName = reader["ProjectName"].ToString();
+                        timesheet.TaskName = reader["TaskName"].ToString();
+                        timesheet.ClientName = reader["ClientName"].ToString();
+                        timesheet.EmployeeName = reader["EmployeeName"].ToString();
+                        timesheetList.Add(timesheet);
+
+
+                    }
+                }
+                reader.Close();
+                connection.Close();
+                return timesheetList;
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message.ToString());
+            }
+        }
+
         public int CreateTimesheet(Timesheet timesheet)
         {
             int returnValue = 0;
--- PayMe/PayMe/Controllers/ReportController.cs
+++ PayMe/PayMe/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
-﻿using DAL;
+﻿using Business;
+using DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,6 +42,51 @@ namespace PayMe.Controllers
             ViewBag.Project = new SelectList(projectManager.GetProjectsByClient(0), "ID", "ProjectName");
             return View();
         }
+
+
+        public ActionResult TimesheetDetail()
+        {
+            TempData["Message"] = null;
+            //ClientManager clientManager = new ClientManager();
+            //ProjectManager projectManager = new ProjectManager();
+            //ViewBag.Client = new SelectList(clientManager.GetClients(), "ID", "ClientName");
+            //ViewBag.Project = new SelectList(projectManager.GetProjectsByClient(0), "ID", "ProjectName");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult TimesheetDetailReport(string FromDate, string ToDate)
+        {
+            IEnumerable<Timesheet> timesheetList = null;
+            try
+            {
+                TimesheetManager timesheetManager = new TimesheetManager();
+                timesheetList = timesheetManager.TimesheetDetailReport(FromDate, ToDate);
+                if (timesheetList.Count() > 0)
+                {
+
+                    var jsonResultS = this.Json(timesheetList, JsonRequestBehavior.AllowGet);
+                    jsonResultS.MaxJsonLength = int.MaxValue;
+                    return jsonResultS;
+                }
+                else
+                {
+
+                    var result = new { Success = "True", Message = "No Data Found" };
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                string sMessage = ex.Message;
+                var result = new { Success = "False", Message = "Exception: " + sMessage };
+                return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }
+
 
         // GET: Report/Create
         public ActionResult Create()
