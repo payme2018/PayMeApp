8b5c8233c9cf57fcc640d5b8ae9609e7bb72ce0a
payme2018 <45126003+payme2018@users.noreply.github.com>
2019-07-04 11:19:13 -0700

    homepage (#21)

---
+++ PayMe/Business/AccountSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Business
+{
+    public class AccountSummary
+    {
+        public int ID { get; set; }
+        [DisplayName("Client Count")]
+        public int ClientCount { get; set; }
+        [DisplayName("Client Count")]
+        public int ProjectCount { get; set; }
+        [DisplayName("Client Count")]
+        public int EmployeeCount { get; set; }
+        [DisplayName("Client Count")]
+        public int  TotalHourIncurrentMonth { get; set; }
+    }
+}
